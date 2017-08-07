016f77eb21867e88507a5cb31d6c793d8a78047f
josephr5000 <josephr5000@yahoo.com>
2017-08-07 09:51:11 -0700

    code complete

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,174 +1,242 @@
 import React, { Component } from 'react';
 import './Calculator.css';
 
+const DigitKey = (props) => {
+  return (
+    <div className={`key key-${props.digit}`} onClick={()=>props.click(props.digit)}>{props.digit}</div>  
+  );
+};
+
+const FunctionKey = (props) => 
+  <div className={`key key-${props.name}`} onClick={()=>props.click(props.name)}>{props.label}</div>;
+
 class Calculator extends Component {
 
   state = {
     display: '0',
-    x: 0,
-    stack: [],
     isDecimal: false,
-    isTyping: false
-  }
-
-  constructor(props) {
-    super(props);
-    this.state.stack = [0];
-  }
-
-  syncX() {
-    this.setState(prevState => ({ x : parseFloat(prevState.display) }));
-  }
-
-  pushStack(n) {
-    this.setState(prevState => ({ stack: [n, ...prevState.stack] }));
-  }
-
-  peekStack(n) {
-    return (this.state.stack.length>0) ? this.state.stack[0] : 0;
-  }
-
-  popStack(n) {
-    this.setState(prevState => ({ stack: prevState.stack.slice(1) }));
-  }
-
-  resetDisplay() {
-    this.setState(prevState => ({
-      display: '0',
-      x: 0,
-      isDecimal: false,
-      isTyping: false
-    }));
-  }
-
-  appendDisplay(d) {
-    this.setState(prevState => ({
-      display: prevState.display=='0' ?
-        d.toString() : prevState.display + d.toString()
-    }));
+    isTyping: true,
+    x: 0,
+    stack: []
   }
 
   inputDigit(d) {
-    if (!this.isTyping) {
-      this.pushStack(this.state.x);
-      this.resetDisplay();
+    if (!this.state.isTyping) {
+      this.setState(prevState => ({
+        display: d,
+        isDecimal: false,
+        isTyping: true,
+      }));
     }
-    this.appendDisplay(d);
-    this.setState({ isTyping: true });
-    this.syncX();
+    else {
+      this.setState(prevState => ({
+        display: (prevState.display === '0') ? d.toString() : prevState.display + d.toString(),
+      }));
+    }
+    this.setState(prevState => ({
+      x: parseFloat(prevState.display)
+    }));
   }
 
   inputDot() {
-    if (!this.isTyping) {
-      this.pushStack(this.state.x);
-      this.resetDisplay();
+    if (!this.state.isTyping) {
+      this.setState(prevState => ({
+        display: '.',
+        isDecimal: true,
+        isTyping: true,
+        stack: [prevState.x, ...prevState.stack]
+      }));
     }
-    if (!this.isDecimal) {
+    else if (!this.state.isDecimal) {
       this.setState(prevState => ({
         display: prevState.display + '.',
         isDecimal: true
       }));
     }
-    this.syncX();
-  }
-
-  inputNeg() {
-    this.setState(prevState => ({
-      display: (prevState.display[0] == '-') ? 
-        prevState.display.slice(1) : '-' + prevState.display
-    }));
-    this.syncX();
   }
 
   inputEnter() {
-    this.pushStack(this.state.x);
-    this.setState({ isTyping: false });
-    this.syncX();
+    if (this.state.isTyping) {
+      this.setState(prevState=>({
+        isTyping: false,
+      }));
+    }
+    this.setState(prevState=>({
+      stack: [prevState.x, ...prevState.stack]
+    }));
+  }
+
+  inputAC() {
+    this.setState({
+      display: '0',
+      isDecimal: false,
+      isTyping: true,
+      x: 0,
+      stack: []
+    });
   }
 
   inputDel() {
-    if (!this.isTyping) {
-      this.pushStack(this.state.x);
-      this.resetDisplay();
-    }
-    this.setState(prevState => {
-      const minLength = (prevState.display[0] == '-') ? 2 : 1;
-      if (prevState.display.length <= minLength) {
-        return {
-          display: '0',
-          isDecimal: false
+    if (this.state.isTyping) {
+      this.setState(prevState=>{
+        const isNegative = prevState.display[0]==='-';
+        const minLength = 1 + (prevState.isDecimal ? 1 : 0) + (isNegative ? 1 : 0);
+        if (prevState.display.length <= minLength) {
+          return { 
+            display: '0',
+            x: 0,
+            isDecimal: false 
+          }
         }
+        else {
+          const newDisplay = prevState.display.slice(0,-1);
+          const newX = parseFloat(newDisplay);
+          return {
+            display: newDisplay,
+            x: newX,
+            isDecimal: !Number.isInteger(newX)
+          }
+        }
+      });
+    }
+  }
+
+  inputNeg() {
+    this.setState(prevState=>{
+      const isNegative = prevState.display[0]==='-';
+      const newDisplay = isNegative ? prevState.display.slice(1) : '-' + prevState.display
+      const newX = parseFloat(newDisplay);
+      return {
+        display: newDisplay,
+        x: newX
       }
-      else {
-        return { display : prevState.display.slice(0,-1) }
-      };
-    })
-    this.syncX();
+    });
+  }
+
+  performDivide() {
+    this.setState(prevState => {
+      const y = (prevState.stack.length) ? prevState.stack[0] : 0;
+      const newX = y / prevState.x;
+      return {
+        display: newX,
+        x: newX,
+        isDecimal: !Number.isInteger(newX),
+        isTyping: false,
+        stack: [newX, ...prevState.stack.slice(1)]
+      }
+    });
+  }
+
+  performMultiply() {
+    this.setState(prevState => {
+      const y = (prevState.stack.length) ? prevState.stack[0] : 0;
+      const newX = y * prevState.x;
+      return {
+        display: newX,
+        x: newX,
+        isDecimal: !Number.isInteger(newX),
+        isTyping: false,
+        stack: [newX, ...prevState.stack.slice(1)]
+      }
+    });
+  }
+
+  performSubtract() {
+    this.setState(prevState => {
+      const y = (prevState.stack.length) ? prevState.stack[0] : 0;
+      const newX = y - prevState.x;
+      return {
+        display: newX,
+        x: newX,
+        isDecimal: !Number.isInteger(newX),
+        isTyping: false,
+        stack: [newX, ...prevState.stack.slice(1)]
+      }
+    });
+  }
+
+  performAdd() {
+    this.setState(prevState => {
+      const y = (prevState.stack.length) ? prevState.stack[0] : 0;
+      const newX = y + prevState.x;
+      return {
+        display: newX,
+        x: newX,
+        isDecimal: !Number.isInteger(newX),
+        isTyping: false,
+        stack: [newX, ...prevState.stack.slice(1)]
+      }
+    });
+  }
+
+  performSwap() {
+    this.setState(prevState => {
+      const newX = (prevState.stack.length) ? prevState.stack[0] : 0;
+      const newY = prevState.x;
+      return {
+        display: newX,
+        x: newX,
+        isDecimal: !Number.isInteger(newX),
+        isTyping: false,
+        stack: [newY, ...prevState.stack.slice(1)]
+      }
+    });
   }
 
   performOperation(op) {
-    this.setState(prevState => {
-      const y = prevState.stack[0];
-      var newX;
-      switch(op) {
-        case '/':
-          newX = y / prevState.x;
-          break;
-        case '*':
-          newX = y * prevState.x;
-          break;
-        case '-':
-          newX = y - prevState.x;
-          break;
-        case '+':
-          newX = y + prevState.x;
-          break;
-        case 'XY':
-          newX = y;
-          break;
-      };
-      let newStack = prevState.stack(1);
-      if (op=='XY') {
-        newStack = [prevState.x, ...newStack];
-      }
-      return {
-        x : newX,
-        stack: newStack,
-      }
-    })
-  }
+    switch(op) {
+      case '/': 
+        this.performDivide();
+        break;
+      case '*':
+        this.performMultiply();
+        break;
+      case '-':
+        this.performSubtract();
+        break;
+      case '+':
+        this.performAdd();
+        break;
+      case 'XY':
+        this.performSwap();
+        break;
+      default:
+        break;
+    }
+  }      
 
   render() {
     return (
       <div className="calculator">
         <div className="display">{this.state.display}</div>
         <div className="functions">
-          <div className="key key-enter" onClick={()=>this.inputEnter()}>E N T E R</div>
-          <div className="key key-del" onClick={()=>this.inputDel()}>DEL</div>
-          <div className="key key-xy" onClick={()=>this.performOperation('XY')}>X-Y</div>
+          <FunctionKey name="del" label="AC" click={()=>this.inputAC()} />
+          <FunctionKey name="xy" label="DEL" click={()=>this.inputDel()} />
+          <FunctionKey name="enter" label="E N T E R" click={()=>this.inputEnter()} />
         </div>
         <div className="digits">
-          <div className="key key-9" onClick={()=>this.inputDigit(9)}>9</div>
-          <div className="key key-8" onClick={()=>this.inputDigit(8)}>8</div>
-          <div className="key key-7" onClick={()=>this.inputDigit(7)}>7</div>
-          <div className="key key-6" onClick={()=>this.inputDigit(6)}>6</div>
-          <div className="key key-5" onClick={()=>this.inputDigit(5)}>5</div>
-          <div className="key key-4" onClick={()=>this.inputDigit(4)}>4</div>
-          <div className="key key-3" onClick={()=>this.inputDigit(3)}>3</div>
-          <div className="key key-2" onClick={()=>this.inputDigit(2)}>2</div>
-          <div className="key key-1" onClick={()=>this.inputDigit(1)}>1</div>
-          <div className="key key-0" onClick={()=>this.inputDigit(0)}>0</div>
-          <div className="key key-dot" onClick={()=>this.inputDot()}>.</div>
-          <div className="key key-chs" onClick={()=>this.inputNeg()}>&plusmn;</div>
+          <DigitKey digit="9" click={(d)=>this.inputDigit(d)} />
+          <DigitKey digit="8" click={(d)=>this.inputDigit(d)} />
+          <DigitKey digit="7" click={(d)=>this.inputDigit(d)} />
+          <DigitKey digit="6" click={(d)=>this.inputDigit(d)} />
+          <DigitKey digit="5" click={(d)=>this.inputDigit(d)} />
+          <DigitKey digit="4" click={(d)=>this.inputDigit(d)} />
+          <DigitKey digit="3" click={(d)=>this.inputDigit(d)} />
+          <DigitKey digit="2" click={(d)=>this.inputDigit(d)} />
+          <DigitKey digit="1" click={(d)=>this.inputDigit(d)} />
+          <DigitKey digit="0" click={(d)=>this.inputDigit(d)} />
+          <FunctionKey name="dot" label="." click={()=>this.inputDot()} />
+          <FunctionKey name="chs" label="&plusmn;" click={()=>this.inputNeg()} />
+
         </div>
         <div className="operators">
-          <div className="key key-div" onClick={()=>this.performOperation('/')}>&divide;</div>
-          <div className="key key-mul" onClick={()=>this.performOperation('*')}>&times;</div>
-          <div className="key key-sub" onClick={()=>this.performOperation('-')}>&minus;</div>
-          <div className="key key-add" onClick={()=>this.performOperation('+')}>+</div>
+          <FunctionKey name="div" label="&divide;" click={()=>this.performOperation('/')} />
+          <FunctionKey name="mul" label="&times;" click={()=>this.performOperation('*')} />
+          <FunctionKey name="sub" label="&minus;" click={()=>this.performOperation('-')} />
+          <FunctionKey name="add" label="+" click={()=>this.performOperation('+')} />
         </div>
         <div className="audit">x = {this.state.x}</div>
-        <div className="audit">stack = [{this.state.stack.join(', ')}]</div>
+        <div className="audit">isDecimal = {this.state.isDecimal.toString()}, isTyping = {this.state.isTyping.toString()}, stack = {JSON.stringify(this.state.stack)}</div>
       </div>  
     )
   }
